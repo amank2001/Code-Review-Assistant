49c66121844599c9c92856045280266b49f71268
amankhan <aman@littlelemon.com>
2025-07-18 11:46:44 +0530

    third commit

--- reviewer/views.py
+++ reviewer/views.py
@@ -2,8 +2,6 @@ import openai
 from django.shortcuts import render
 from django.conf import settings
 
-openai_api_key = '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'
-openai.api_key = openai_api_key
 
 def review_code(request):
     if request.method == 'POST':
